099802084ec824abf05b14901c1d9bbd4c6a7b58
root <root@docker-test.novalocal>
2019-01-24 05:36:45 +0000

    adding scripts to run strintie

--- scripts/stringtie.sh
+++ scripts/stringtie.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 echo "Running Stringtie"
-sleep 25s
+for file in /datasets/*.bam;do
+	echo ${file##*/}
+	./stringtie/stringtie -p 8 -G  /datasets/annotB36.gtf  -o /outputs/${file##*/}.gtf  ${file}
+done
+sleep 30s
